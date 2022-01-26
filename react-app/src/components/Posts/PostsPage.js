f11828086f685c08ed3e86e035eb2bffb91e1dec
Chloe-Baylock <chloebox523@gmail.com>
2022-01-26 18:42:51 -0500

    show posts page integrated

--- react-app/src/components/Posts/PostsPage.js
+++ react-app/src/components/Posts/PostsPage.js
@@ -1,43 +1,7 @@
-import React, { useState, useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { getAllPosts, destroyPost } from '../../store/posts';
-// import EditPost from './EditPost'
+import React from 'react';
 import ShowPosts from './ShowPosts'
 
 function PostsPage() {
-
-  // const dispatch = useDispatch();
-
-  // const [posts, setPosts] = useState('')
-  // const [users, setUsers] = useState('')
-  // const [trigger, setTrigger] = useState(-2)
-  // const [flicker, setFlicker] = useState(false)
-  // const [editButton, setEditButton] = useState('Edit')
-
-  // const deletePost = async post => {
-  //   console.log('deleting post', post.id)
-  //   await dispatch(destroyPost(post));
-  //   setFlicker(!flicker)
-  // }
-
-  // useEffect(() => {
-  //   const fetchData = async function () {
-  //     if (!users) {
-  //       const response = await fetch('/api/users/');
-  //       const data = await response.json();
-  //       const setto = data.users
-  //       setUsers(setto);
-  //     }
-  //     const allPosts = await dispatch(getAllPosts());
-  //     setPosts(allPosts);
-  //   }
-  //   fetchData();
-  // }, [dispatch, flicker])
-
-  // const retEdit = post => {
-  //   if (editButton.postId === post.id) return true
-  //   else return false;
-  // }
 
   return (
     <div>
