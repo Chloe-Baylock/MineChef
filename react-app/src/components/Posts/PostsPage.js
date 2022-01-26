b62a95d913c2db3eceed42119e4cbe48532bc581
Chloe-Baylock <chloebox523@gmail.com>
2022-01-26 18:19:08 -0500

    retEdit

--- react-app/src/components/Posts/PostsPage.js
+++ react-app/src/components/Posts/PostsPage.js
@@ -6,10 +6,11 @@ function PostsPage() {
 
   const dispatch = useDispatch();
 
-  const [ posts, setPosts ] = useState('')
-  const [ users, setUsers ] = useState('')
-  const [ trigger, setTrigger ] = useState(-2)
-  const [ flicker, setFlicker ] = useState(false)
+  const [posts, setPosts] = useState('')
+  const [users, setUsers] = useState('')
+  const [trigger, setTrigger] = useState(-2)
+  const [flicker, setFlicker] = useState(false)
+  const [editButton, setEditButton] = useState('Edit')
 
   const deletePost = async post => {
     console.log('deleting post', post.id)
@@ -18,7 +19,7 @@ function PostsPage() {
   }
 
   useEffect(() => {
-    const fetchData = async function() {
+    const fetchData = async function () {
       if (!users) {
         const response = await fetch('/api/users/');
         const data = await response.json();
@@ -31,30 +32,55 @@ function PostsPage() {
     fetchData();
   }, [dispatch, flicker])
 
+  // const retEdit = post => {
+  //   if (editButton.postId === post.id) return 'Cancel'
+  //   else return 'Edit';
+  // }
+  const retEdit = post => {
+    if (editButton.postId === post.id) return true
+    else return false;
+  }
+
   return (
     <div>
       <h1>Posts</h1>
       <ul>
-        {posts && posts.map(post => (
-          <li key={post.id}>{post.title} 
-          by {users.filter(user => user.id === post.author_id)[0].username}
-          {+trigger === post.id && (
-          <EditPost
-            post = {post}
-            setTrigger = {setTrigger}
-            flicker = {flicker}
-            setFlicker = {setFlicker}
-          />)}
-          <button id={post.id}
-            onClick={e => {
-              // logme(e.target.id)
-              console.log('e.target.id is', +e.target.id)
-              setTrigger(e.target.id)
-            }}
-          >Edit</button>
-          <button
-            onClick={() => deletePost(post)}
-          >Delete</button></li>
+        {posts && posts.map(post =>  (
+          <li key={post.id}>{post.title}
+            by {users.filter(user => user.id === post.author_id)[0].username}
+            {+trigger === post.id && (
+              <EditPost
+                post={post}
+                setTrigger={setTrigger}
+                flicker={flicker}
+                setFlicker={setFlicker}
+              />)}
+            <button id={post.id}
+              onClick={e => {
+                console.log('post.id is', post.id)
+                if (retEdit(post)) {
+                  console.log('button read cancel now reads edit')
+                  console.log(editButton)
+                  setEditButton('Edit')
+                  setTrigger(-4);
+                }
+                else if (editButton === 'Edit') {
+                  console.log('case 2')
+                  console.log(editButton)
+                  setEditButton({ postId: post.id })
+                  setTrigger(e.target.id);
+                }
+                else {
+                  console.log('case 3 diff button reads cancel')
+                  setEditButton({ postId: post.id })
+                  setTrigger(e.target.id);
+                }
+              }}
+            >{retEdit(post) && 'Cancel' || 'Edit'}</button>
+            {/* >{retEdit(post)}</button> */}
+            <button
+              onClick={() => deletePost(post)}
+            >Delete</button></li>
         ))}
       </ul>
     </div>
