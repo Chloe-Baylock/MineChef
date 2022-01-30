642b60d7751413499c9c072bec250f745f9028fd
Chloe-Baylock <chloebox523@gmail.com>
2022-01-30 13:47:59 -0500

    okay post page

--- react-app/src/components/Posts/PostPage/PostPage.js
+++ react-app/src/components/Posts/PostPage/PostPage.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getAllPosts } from '../../../store/posts';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllPosts, destroyPost } from '../../../store/posts';
 import { useHistory, useParams } from 'react-router-dom';
+import { PencilIcon, TrashIcon } from "@heroicons/react/solid";
+import EditPost from '../EditPost';
 import './PostPage.css'
 
 function PostPage() {
   const history = useHistory();
+  const currentUser = useSelector(state => state.session.user)
 
   const dispatch = useDispatch();
   const { postId } = useParams();
   const [post, setPost] = useState('');
   const [author, setAuthor] = useState('');
+  const [trigger, setTrigger] = useState(-2)
+  const [editButton, setEditButton] = useState('Edit')
+  const [flicker, setFlicker] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,7 +24,7 @@ function PostPage() {
       setPost(allPosts.filter(post => post.id === +postId)[0])
     }
     fetchData();
-  }, [dispatch, postId])
+  }, [dispatch, postId, flicker])
 
   useEffect(() => {
     if (post) {
@@ -30,11 +36,61 @@ function PostPage() {
     }
   }, [post])
 
+
+  const deletePost = async post => {
+    await dispatch(destroyPost(post));
+    history.push('/profile');
+  }
+
+  const retEdit = post => {
+    if (editButton.postId === post.id) return true
+    else return false;
+  }
+
+  const isRightUser = (post) => {
+    if (currentUser.id === post.author_id) return 'post-page-owner'
+    else return 'post-page-not-owner';
+  }
+
   return (
     <>
       <div className='post-page-below-nav'>
         <div className='post-page-centered-fill'>
           <div className='post-page-top-part'>
+            <div className='post-page-edit-delete'>
+              <PencilIcon
+                className={isRightUser(post)}
+                id='post-page-pencil'
+                onClick={e => {
+                  if (retEdit(post)) {
+                    setEditButton('Edit');
+                    setTrigger(-4);
+                  }
+                  else if (editButton === 'Edit') {
+                    setEditButton({ postId: post.id });
+                    setTrigger(1);
+                  }
+                  else {
+                    setEditButton({ postId: post.id });
+                    setTrigger(1);
+                  }
+                }}
+              />
+              <TrashIcon
+                className={isRightUser(post)}
+                id='post-page-delete'
+                onClick={() => deletePost(post)}
+              />
+            </div>
+            {+trigger > 0 && (
+              <EditPost
+                post={post}
+                setEditButton={setEditButton}
+                setTrigger={setTrigger}
+                flicker={flicker}
+                setFlicker={setFlicker}
+              />
+            )}
             <p
               className='post-page-author-p'
               onClick={() => history.push(`/users/${author.id}`)}
