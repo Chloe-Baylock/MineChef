77b208a842e7f70faca13de0981bda8318bad02c
Chloe-Baylock <chloebox523@gmail.com>
2022-01-30 21:22:18 -0500

    almost done

--- react-app/src/components/Error404Page/Error404page.js
+++ react-app/src/components/Error404Page/Error404page.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './Error404Page.css'
 
 function Error404Page() {
-  const minewithahat = 'https://i.ibb.co/SsYtLQN/minewithahat.png';
   return (
-    <>
-      <h1>
-        Error 404, We could not find the page you were looking for.
-      </h1>
-      <img className='emily-image' src='https://minechef.s3.amazonaws.com/cb039736185348d4a1a8a2a8f016a79e.png' alt='minewithahat'></img>
-    </>
+    <div className='err-bg-color'>
+      <div className='err-404-page'>
+        <h1 className='err-h1'>
+          Error 404, We could not find the page you were looking for.
+        </h1>
+        <img className='emily-image' src='https://minechef.s3.amazonaws.com/cb039736185348d4a1a8a2a8f016a79e.png' alt='minewithahat'></img>
+      </div>
+    </div>
   )
 }
 
